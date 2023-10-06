3e5bca51018113b193afdd50787d4950ced13e52
Peter <pfreche@gmail.com>
2023-10-06 19:54:01 +0200

    add gap in menu; add settings attributes

--- src/app/model/DBid.ts
+++ src/app/model/DBid.ts
@@ -3,6 +3,7 @@ export class DBid { // A Descriptive Bid
   bid: string;
   desc: string | undefined;
   con: string;
+  settings: string | undefined;
 
   constructor(bid: string, con: string) {
     this.bid = bid;
